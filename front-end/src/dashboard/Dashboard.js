e41bbf294f58bbec574864a084316994ae3c962c
Rume <Sprtrks@gmail.com>
2024-03-14 20:11:23 -0400

    Frontend dashboard

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { useHistory } from "react-router-dom";
+import { previous, next } from "../utils/date-time";
+import TablesList from "../tables/TablesList";
+import ReservationList from "../reservations/ReservationList";
+import { today } from "../utils/date-time";
+import useQuery from "../utils/useQuery";
 
 /**
  * Defines the dashboard page.
@@ -8,31 +14,73 @@ import ErrorAlert from "../layout/ErrorAlert";
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
+
 function Dashboard({ date }) {
+  let query = useQuery();
+  date = query.get("date") || date;
+
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  const [currentDate, setCurrentDate] = useState(date);
+  const [ tables, setTables] = useState([]);
+  const [TablesError, setTablesError] = useState(null);
+  const history = useHistory();
 
-  useEffect(loadDashboard, [date]);
+  useEffect(loadDashboard, [currentDate]);
 
   function loadDashboard() {
     const abortController = new AbortController();
     setReservationsError(null);
-    listReservations({ date }, abortController.signal)
+    listReservations({ date: currentDate }, abortController.signal)
       .then(setReservations)
       .catch(setReservationsError);
     return () => abortController.abort();
   }
 
+  //For date change
+  const previousDayHandler = () => {
+    setCurrentDate(previous(currentDate));
+    history.push(`/dashboard?date=${previous(currentDate)}`);
+  }
+
+  const nextDayHandler = () => {
+    setCurrentDate(next(currentDate));
+    history.push(`/dashboard?date=${next(currentDate)}`);
+  }
+
+  const presentDayHandler = () => {
+    setCurrentDate(today());
+    history.push(`/dashboard`);
+  }
+
+  //html layout of Dashboard
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+        <h4 className="mb-0">Reservations for {currentDate}</h4>
       </div>
+      <div className="item centered">
+        <div className="group-row">
+          <button className="btn btn-dark btn-md" onClick={previousDayHandler}>
+            Previous
+          </button>
+          <button className="btn btn-dark btn-md" onClick={todayHandler}>
+            Today
+          </button>
+          <button className="btn btn-dark btn-md" onClick={nextDayHandler}>
+            Next
+          </button>
+        </div>
+      </div>
+      <ReservationList reservations={reservations} />
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <TableList tables={tables} />
+      <ErrorAlert error={tablesError} />
+      {/* {JSON.stringify(reservations)} */}
     </main>
   );
 }
+
 
 export default Dashboard;

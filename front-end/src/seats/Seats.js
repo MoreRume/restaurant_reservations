9e87e6ef11505ef3cb0f7cb84d7edbcc57667a07
Rume <Sprtrks@gmail.com>
2024-03-19 13:11:49 -0400

    Seqts.js

---
+++ front-end/src/seats/Seats.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import { listTables, updateSeat } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
+
+function Seats() {
+
+    const history = useHistory();
+    const { reservation_id } = useParams();
+    const [tables, setTables] = useState([]);
+    const [tablesError, setTableError] = useState([]);
+    const [tableId, setTableId] = useState(0);
+
+    useEffect(loadTables, []);
+
+    function loadTables() {
+        const abortController = new AbortController();
+        setTableError(null);
+        listTables(abortController.signal)
+        .then(setTables)
+        .catch(setTableError);
+        return () => abortController.abort();
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const abortController = new AbortController();
+        updateSeat(tableId, reservation_id, abortController.signal)
+        .then(() => history.push("/"))
+        .catch(setTableError);
+        return () => abortController.abort();
+    };
+
+    function changeHandler (e){
+        e.preventDefault();
+        setTableId(e.target.value);
+    };
+
+    function cancelHandler(e){
+        e.preventDefault();
+        history.push("/");
+    };
+
+    const tableList = tables.map((table) =>{
+        return(
+            <option key={table.table_id} value={table.table_id}>
+                {table.table_name} - {table.capacity}
+            </option>
+        );
+    });
+
+    return(
+        <div>
+            <h1>Seat Reservation</h1>
+            <ErrorAlert error={tablesError} />
+            <form className="d-flex">
+                <select
+                id="table_id"
+                name="table_id"
+                required={true}
+                onChange={changeHandler}
+                >
+                    <option defaultValue>Select a table</option>
+                    {tableList}
+                </select>
+                <button type="submit" className="btn btn-primary btn-md" onClick={submitHandler} >Submit</button>
+                <button type="reset" className="btn brn-danger btn-md" onClick={cancelHandler}>Cancel</button>
+            </form>
+        </div>
+    );
+}
+
+export default Seats;

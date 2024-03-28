cb364b4964401c64cca71f1469e69e4d2348167e
Rume <Sprtrks@gmail.com>
2024-03-28 15:48:36 -0400

    can't name menu menu. Stupid test

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -17,7 +17,7 @@ function Menu() {
           to="/"
         >
           <div className="sidebar-brand-text mx-3">
-            <span>Menu</span>
+            <span>Periodic tables</span>
           </div>
         </Link>
         <hr className="sidebar-divider my-0" />
